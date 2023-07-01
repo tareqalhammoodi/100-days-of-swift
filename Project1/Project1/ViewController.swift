ee1f30b94809711f061ec4ce75bd29bf392ed6c3
Tareq Alhammoodi <44919941+tareqalhammoodi@users.noreply.github.com>
2023-07-01 22:42:34 +0300

    initial commit

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Tareq Alhammoodi on 1.07.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+}
+
